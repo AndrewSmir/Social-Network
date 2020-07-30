fcf5102f8443bfe8808f27c5419c7d0536d60752
AndrewSmir <49587118+AndrewSmir@users.noreply.github.com>
2020-07-30 11:28:34 +0300

    Project created

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav(){
+    return (<nav className='nav'>
+        <ul>
+            <li className='item'><a href="#">Profile</a></li>
+            <li className='item'><a href="#">Messages</a></li>
+            <li className='item'><a href="#">News</a></li>
+            <li className='item'><a href="#">Music</a></li>
+            <li className='item'><a href="#">Settings</a></li>
+        </ul>
+    </nav>)
+}
+
+export default Nav;
+
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Profile(){
+    return(
+        <div className='content'>
+            <img
+                src="https://process.images.nathab.com/A6dTpd53SmIg0pBfJJhgAz/resize=width:864/quality=value:60/cache=expiry:31536000/compress/https://www.nathab.com/uploaded-files/carousels/HERO/South-America/83-ALT.jpg"
+                alt="IMG"/>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+            </div>
+            <div>
+                post1
+            </div>
+            <div>
+                post2
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
